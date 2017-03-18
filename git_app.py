66b0d0f866d0bbf5e7840c1795b37060440b308c
Pavana Achar <pavana.s.achar1991@gmail.com>
2017-03-17 18:33:50 -0700

    assignment1

--- git_app.py
+++ git_app.py
@@ -1,6 +1,6 @@
 import sys
 from github import Github
-from flask import Flask,jsonify
+from flask import Flask,jsonify,abort,make_response
 import yaml,json
 
 app=Flask(__name__)
@@ -9,21 +9,35 @@ path =  sys.argv[1].split("/")
 user = path[3]
 repo = path[4]
 
-global d
 
 @app.route("/v1/<filename>")
-
 def hello(filename):
 	
-	fileContent = Github().get_user(user).get_repo(repo).get_contents(filename).decoded_content
+	try:
+		fname = filename.split(".")[0]
+		fname= fname+".yml"
 
-	if filename.endswith('.yml'):
-		return yaml.load(msg)	
+		fileContent = Github().get_user(user).get_repo(repo).get_file_contents(fname).decoded_content
 
-	if filename.endswith('.json'):	
-		d =  json.loads(msg)
-		return jsonify(d) 
+		if filename.endswith('.yml'):
+			return fileContent 
 
+		elif filename.endswith('.json'):	
+			return json.dumps(yaml.load(fileContent),sort_keys=True,indent=2)
+
+		else:
+			return "Invalid file type"
+		
+		
+	except:
+		abort(404)
+
+@app.errorhandler(404)
+def FileNotFound(e):
+	return make_response("error : file not found")
+	
+
+		
 
 if __name__ == "__main__":
 	app.run(debug=True,host='0.0.0.0')
