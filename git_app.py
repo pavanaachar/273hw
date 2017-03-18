2da3eb09a2b22fd7d0c698f08e1592a128ede3dc
Pavana Achar <pavana.s.achar1991@gmail.com>
2017-03-17 21:37:21 -0700

    Assignment1 : Flask application to fetch and display the contents of config-repo

--- git_app.py
+++ git_app.py
@@ -1,44 +1,56 @@
 import sys
-from github import Github
+from github import Github, UnknownObjectException
 from flask import Flask,jsonify,abort,make_response
 import yaml,json
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-path =  sys.argv[1].split("/")
+path = sys.argv[1].split("/")
 user = path[3]
 repo = path[4]
 
+errstr = ""
 
 @app.route("/v1/<filename>")
 def hello(filename):
-	
+	global errstr
 	try:
-		fname = filename.split(".")[0]
-		fname= fname+".yml"
+		fileContent = Github().get_user(user).get_repo(repo).get_file_contents(filename).decoded_content
 
-		fileContent = Github().get_user(user).get_repo(repo).get_file_contents(fname).decoded_content
-
-		if filename.endswith('.yml'):
+		if filename.endswith('.yml') or filename.endswith('.json'):
 			return fileContent 
-
-		elif filename.endswith('.json'):	
-			return json.dumps(yaml.load(fileContent),sort_keys=True,indent=2)
-
 		else:
-			return "Invalid file type"
+			print "hello"
+			raise TypeError()
 		
-		
-	except:
+	except UnknownObjectException:
+		errstr = "ERROR : File not found."
 		abort(404)
+
+	except TypeError:
+		errstr = "ERROR : Invalid File Type."
+		abort(500)
+	
+	except Exception as e:
+		print e
+		errstr = "ERROR : Generic error."
+		abort(500)
+
+
+def GetRespObj(errcode):
+        global errstr
+        r = make_response(errstr, errcode)
+        errstr = ""
+        return r
 
 @app.errorhandler(404)
 def FileNotFound(e):
-	return make_response("error : file not found")
-	
+        return GetRespObj(404)
 
-		
+@app.errorhandler(500)
+def GenericError(e):
+        return GetRespObj(500)
+
 
 if __name__ == "__main__":
-	app.run(debug=True,host='0.0.0.0')
-	
+	app.run(debug=True, host='0.0.0.0')
