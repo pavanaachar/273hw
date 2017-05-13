da5866996441d80801d25c80081964d12244c691
Pavana Achar <pavana.s.achar1991@gmail.com>
2017-05-12 19:55:40 -0700

    changing the invocation to update_item

--- Assignment2/PizzaMenuPut/service.py
+++ Assignment2/PizzaMenuPut/service.py
@@ -4,12 +4,26 @@ import boto3
 import json
 from botocore.exceptions import ClientError
 
+def update_item(table, key_name, key_val, attr_name, attr_val):
+    table.update_item(
+                      Key = {
+                          key_name: key_val,
+                      },
+                      UpdateExpression='SET ' + attr_name + ' = :val1',
+                      ExpressionAttributeValues={
+                          ':val1': attr_val
+                      })
+
 def handler(event, context):
     table = boto3.resource('dynamodb', region_name='us-west-2').Table('PizzaMenu')
     try:
-        table.put_item(Item={"menu_id": event["menu_id"],
-                             "selection" : event["selection"]
-                               })
+        update_item(table=table,
+                    key_name = "menu_id",
+                    key_val = event["menu_id"],
+                    attr_name = "selection",
+                    attr_val = event["selection"]
+                   )
+
     except Exception, e:
         raise Exception("Error: %s" % e.message)
     return "OK"
