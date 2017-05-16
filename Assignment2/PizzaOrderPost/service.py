dc750c46efaaaa0670b21a693a97cf85b09ce1d7
Pavana Achar <pavana.s.achar1991@gmail.com>
2017-05-15 22:14:40 -0700

    Modifying output format

--- Assignment2/PizzaOrderPost/service.py
+++ Assignment2/PizzaOrderPost/service.py
@@ -9,21 +9,26 @@ def handler(event, context):
     order_table = boto3.resource('dynamodb', region_name='us-west-2').Table('PizzaOrder')
     menu_table = boto3.resource('dynamodb', region_name='us-west-2').Table('PizzaMenu')
     try:
-        order_table.put_item(Item={"order_id": event["order_id"],
-                             "menu_id": event["menu_id"],
-                             "customer_name" : event["customer_name"],
-                             "customer_email": event["customer_email"]
+        order_table.put_item(Item={'order_id': event['order_id'],
+                             'menu_id': event['menu_id'],
+                             'customer_name' : event['customer_name'],
+                             'customer_email': event['customer_email'],
+                             'size': 'null',
+                             'selection': 'null',
+                             'order_time': 'null',
+                             'order_status': 'null',
+                             'costs': 'null'
                              })
         response = menu_table.get_item(Key = {
                                          'menu_id': event['menu_id']
                                          })
         item = response['Item']
         selection = item['selection']
-        message = "Hi"+ event["customer_name"]+" , please choose one of these selection:"
+        message = "Hi "+ event["customer_name"] + ", please choose one of these selection: "
         for i in range(1, len(selection)+1):
             message += str(i) + ". " + selection[i-1]
             if i < len(selection):
-               message += ','  
+               message += ', '  
         resp_dict['message'] = message
 
     except Exception, e:
